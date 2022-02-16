f643fa1766c2d16b4ce7169ceda1aa3c6df18695
Artem <artem-tikhonov-90@list.ru>
2022-02-16 14:04:41 +0300

    10l_ТихоновАртем

--- weatherLesson1/weatherLesson1/ViewController.swift
+++ weatherLesson1/weatherLesson1/ViewController.swift
@@ -9,19 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let redView = UIView()
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(redView)
-        redView.backgroundColor = .red
-        
     }
-
-    override func viewDidLayoutSubviews() {
-        redView.frame = CGRect(x: 140, y: 300, width: 60, height: 60)
+    
+    override func viewWillAppear(_ animated:Bool){
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
-
 }
 
